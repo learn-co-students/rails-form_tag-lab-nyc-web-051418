f3de95ad795f4bd99cf503c0affc15efd82a55ce
Ledio <L.Bode515@gmail.com>
2018-06-11 14:02:33 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,4 +8,33 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-end
+  def new
+    @student = Student.new
+  end
+
+    def create
+      session[:form_params] = params.inspect
+      redirect_to new_student_path
+    end
+
+    private
+
+      def set_student
+        @student = Student.find(params[:id])
+      end
+  end
+  
+#   def create
+#       @student = Student.create(student_params)
+#         if @student.save
+#         link_to new_student_path
+#         else
+#           flash.now[:error] = "Could not save student"
+#           render "new"
+#         end
+#     end
+#
+#    def student_params
+#     params.require(:student).permit(:first_name, :last_name)
+#   end
+# end
